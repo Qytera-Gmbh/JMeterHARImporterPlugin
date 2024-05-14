38fa67cfec496c16f641dec8d6daf34109600f43
Sebastian Vollbrecht <sebivo@freenet.de>
2024-05-14 14:23:12 +0200

    Add option to exclude certain hosts

---
+++ src/test/java/de/qytera/jmeterharimporter/HARImporterIgnoreHostTest.java
@@ -0,0 +1,48 @@
+package de.qytera.jmeterharimporter;
+
+import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.tree.TreeNode;
+
+import static org.junit.Assert.assertEquals;
+
+// tests ignored hosts
+public class HARImporterIgnoreHostTest {
+
+    HARImporter harImporter = null;
+
+    @Before
+    public void setUp() {
+        harImporter = new HARImporter("src/test/resources/filter/filter-host.har");
+    }
+
+    @Test
+    public void testHARImporter_ignoreHostsEmpty() {
+        TreeNode threadGroup = harImporter.addNewThreadGroupWithSamplers();
+        assertEquals(4, threadGroup.getChildCount());
+        assertEquals("jsonplaceholder.typicode.com", NodeUtil.getChild(threadGroup.getChildAt(0), HTTPSamplerProxy.class).getDomain());
+        assertEquals("jsonplaceholder.typicode.com", NodeUtil.getChild(threadGroup.getChildAt(1), HTTPSamplerProxy.class).getDomain());
+        assertEquals("example.org", NodeUtil.getChild(threadGroup.getChildAt(2), HTTPSamplerProxy.class).getDomain());
+        assertEquals("example.org", NodeUtil.getChild(threadGroup.getChildAt(3), HTTPSamplerProxy.class).getDomain());
+    }
+
+    @Test
+    public void testHARImporter_ignoreSingleHost() {
+        harImporter.ignoreHost("example.org");
+        TreeNode threadGroup = harImporter.addNewThreadGroupWithSamplers();
+        assertEquals(2, threadGroup.getChildCount());
+        assertEquals("jsonplaceholder.typicode.com", NodeUtil.getChild(threadGroup.getChildAt(0), HTTPSamplerProxy.class).getDomain());
+        assertEquals("jsonplaceholder.typicode.com", NodeUtil.getChild(threadGroup.getChildAt(1), HTTPSamplerProxy.class).getDomain());
+    }
+
+    @Test
+    public void testHARImporter_ignoreAllHosts() {
+        harImporter.ignoreHost("example.org");
+        harImporter.ignoreHost("jsonplaceholder.typicode.com");
+        TreeNode threadGroup = harImporter.addNewThreadGroupWithSamplers();
+        assertEquals(0, threadGroup.getChildCount());
+    }
+
+}
